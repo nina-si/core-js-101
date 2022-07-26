b405e03c2e0ead8e19b4cd5ecf9f5bb4db030564
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-07-26 11:24:57 +0200

    feat: finish 03-array-tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -572,8 +572,11 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  const index = indexes[0];
+  if (indexes.length === 1) return arr[index];
+  indexes.shift();
+  return getElementByIndexes(arr[index], indexes);
 }
 
 /**
